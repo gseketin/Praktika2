19d31c2d1541238413fe35001675c1dfcf860fb1
gseketin <gseketin@yandex.ru>
2020-04-13 16:59:01 +0300

    5, 6, 7 Tasks

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$page_title='dsfsefedf';
+require_once('functions.php');
+$page_content = compile_template('index.php',
+    ['arr' => $arr,
+        'advertisement' => $advertisement]);
+
+$layout_content = compile_template( 'layout.php',
+    ['page_title'   => 'Главная страница', 'is_auth' => $is_auth,
+        'user_avatar' => $user_avatar, 'user_name' => $user_name,
+        'page_content'  => $page_content,
+        'arr' => $arr
+    ]);
+
+print($layout_content);
+?>
